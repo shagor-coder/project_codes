700f411a4aadf0b8bbff3f45cc125d9919e74ca2
shagor-coder <msh43320@gmail.com>
2023-12-03 22:25:55 +0600

    Updated Code

--- annuity_facts_now/get_info_by_email/middlewares/get_webhook_data.js
+++ annuity_facts_now/get_info_by_email/middlewares/get_webhook_data.js
@@ -1,7 +1,7 @@
 function get_webhook_data(req, res) {
   const { first_name, last_name, email, location, customData } = req.body;
 
-  if (!first_name || !last_name || !email || !location || !customData) {
+  if (!first_name || !email || !location || !customData) {
     return res
       .status(400)
       .json({ error: "One or more required fields are missing in req.body" });
--- annuity_facts_now/get_info_by_email/middlewares/update_contact_info.js
+++ annuity_facts_now/get_info_by_email/middlewares/update_contact_info.js
@@ -1,95 +1,63 @@
-const { default: axios } = require('axios');
-const FormData = require('form-data');
-const change_info_text_case = require('./change_info_case');
+function process_atd_info(request, response) {
+  const {
+    postal_address,
+    net_worth,
+    rfm_online_avg_days,
+    household_income,
+    home_owner_status,
+    rfm_offline_avg_days,
+    home_market_value,
+    length_of_residence,
+    rfm_avg_dollars,
+  } = request.atd_response_data;
 
-async function update_contact_info(request, response) {
-	const { email, location_id, surveyId, checkout_url, location_name } =
-		request.formatted_data;
+  if (!postal_address) {
+    const atd_info = {
+      postal_first_name: "",
+      postal_last_name: "",
+      address: "",
+      city: "",
+      state: "",
+      zip: "",
+      household_income: household_income ? household_income : "",
+      net_worth: net_worth ? net_worth : "",
+      home_owner_status: home_owner_status ? home_owner_status : "",
+      home_market_value: home_market_value ? home_market_value : "",
+      length_of_residence: length_of_residence ? length_of_residence : "",
+      rfm_online_avg_days: rfm_online_avg_days ? rfm_offline_avg_days : "",
+      rfm_offline_avg_days: rfm_offline_avg_days ? rfm_offline_avg_days : "",
+      rfm_avg_dollars: rfm_avg_dollars ? rfm_avg_dollars : "",
+    };
+    return (request.atd_info = atd_info);
+  } else {
+    const {
+      first_name,
+      last_name,
+      address,
+      city,
+      state,
+      zip: long_zip,
+    } = postal_address;
 
-	const fData = new FormData();
+    const atd_info = {
+      postal_first_name: first_name ? first_name : "",
+      postal_last_name: last_name ? last_name : "",
+      address: address ? address : "",
+      city: city ? city : "",
+      state: state ? state : "",
+      zip: long_zip ? long_zip : "",
+      household_income: household_income ? household_income : "",
+      net_worth: net_worth ? net_worth : "",
+      home_owner_status: home_owner_status ? home_owner_status : "",
+      home_market_value: home_market_value ? home_market_value : "",
+      length_of_residence: length_of_residence ? length_of_residence : "",
+      rfm_online_avg_days: rfm_online_avg_days ? rfm_offline_avg_days : "",
+      rfm_offline_avg_days: rfm_offline_avg_days ? rfm_offline_avg_days : "",
+      rfm_avg_dollars: rfm_avg_dollars ? rfm_avg_dollars : "",
+    };
 
-	const {
-		postal_first_name,
-		postal_last_name,
-		address,
-		city,
-		state,
-		zip,
-		household_income,
-		net_worth,
-		home_owner_status,
-		home_market_value,
-		length_of_residence,
-		rfm_online_avg_days,
-		rfm_offline_avg_days,
-		rfm_avg_dollars,
-	} = request.atd_info;
-
-	const atd_info_obj = {
-		postal_first_name,
-		postal_last_name,
-		address,
-		city,
-		state,
-		zip,
-		household_income,
-		net_worth,
-		home_owner_status,
-		home_market_value,
-		length_of_residence,
-		rfm_online_avg_days,
-		rfm_offline_avg_days,
-		rfm_avg_dollars,
-	};
-
-	const eventData = {
-		source: 'direct',
-		page: {
-			url: checkout_url,
-			title: location_name,
-		},
-		timestamp: new Date().getTime(),
-		type: 'page-visit',
-		domain: 'https://www.annuityanswersnow.com/',
-	};
-
-	let formData = {
-		email: email,
-		formId: surveyId.trim(),
-		location_id: location_id.trim(),
-		eventData: eventData,
-	};
-
-	const keys = Object.keys(atd_info_obj);
-
-	keys.forEach((key) => {
-		if (!atd_info_obj[key] || atd_info_obj[key] === '') return;
-		if (key === 'state') return (formData[key] = atd_info_obj[key]);
-		formData[key] = change_info_text_case(atd_info_obj[key]);
-	});
-
-	fData.append('formData', JSON.stringify(formData));
-
-	try {
-		let config = {
-			method: 'post',
-			maxBodyLength: Infinity,
-			url: 'https://services.leadconnectorhq.com/surveys/submit',
-			headers: {
-				...fData.getHeaders(),
-			},
-			data: fData,
-		};
-		const contact = await axios.request(config);
-		return response.status(200).json({
-			message: 'The contact successfully updated!!',
-			contact: contact.data,
-		});
-	} catch (error) {
-		return response.status(500).json({
-			message: 'Contact not updated!!',
-		});
-	}
+    return (request.atd_info = atd_info);
+  }
 }
 
-module.exports = update_contact_info;
+module.exports = process_atd_info;
