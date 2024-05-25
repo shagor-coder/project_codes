8c93e0a3b52bb68f65d406c890d7672cb50df4ee
shagor-coder <msh43320@gmail.com>
2024-05-26 01:43:50 +0600

    Updated

--- rohan_attravanam/send_email_with_opp/middlewares/clear-opportunity.js
+++ rohan_attravanam/send_email_with_opp/middlewares/clear-opportunity.js
@@ -1,3 +1,20 @@
-const clearOpportunity = (request, response) => {};
+const db = require("../firebase/app");
 
-module.exports = clearOpportunity;
+const clearOpportunities = async (request, response) => {
+  try {
+    const { location } = request.body;
+    if (!location)
+      return response.status(4043).json({ message: "No Location provided!" });
+    const { id: locationId } = location;
+    await db
+      .collection("opportunities")
+      .doc("locationId", "==", locationId.toString().trim())
+      .delete();
+
+    response.status(200).json({ message: "Successfully deleted" });
+  } catch (error) {
+    response.status(500).json({ message: "Something went wrong!" });
+  }
+};
+
+module.exports = clearOpportunities;
